657fda578939c70d2c80ee1e201f20b60a9421d0
ALFA309 <alfastreaming095@gmail.com>
2025-06-08 02:59:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+var typed = new Typed(".typing",{
+    strings: ["","Web Developer","UI/UX Desingneer","Software Engineer"],
+    typeSpeed: 80,
+    backSpeed: 80,
+});
+
+var typed = new Typed(".typing2",{
+    strings: ["UI/UX Desingneer","Web Developer","Software Engineer"],
+    typeSpeed: 100,
+    backSpeed: 80,
+});
+
+// Show/hide Ans 
+
+const faqs = document.querySelectorAll('.fag');
+faqs.forEach(fag =>{
+    fag.addEventListener('click',()=>{
+        fag.classList.toggle('open');
+
+        //Change Icon
+
+        const icon = fag.querySelector('.fag_icon i');
+        if(icon.className === 'fa-solid fa-plus'){
+            icon.className = 'fa-solid fa-minus'
+        }else{
+            icon.className = 'fa-solid fa-plus'
+        }
+    })
+})
+
+
+// open nav bar 
+
+const menu = document.querySelector('.nav_menu');
+const menuBtn1 = document.querySelector('#open-menu-btn');
+const menuBtn2 = document.querySelector('#close-menu-btn');
+
+menuBtn1.addEventListener('click',()=>{
+    menu.style.display = "flex";
+    menuBtn2.style.display = "inline-block";
+    menuBtn1.style.display = "none";
+})
+
+// Colose Nav menu
+
+const closeNav = ()=>{
+    menu.style.display = "none";
+    menuBtn2.style.display = "none"
+    menuBtn1.style.display = "inline-block";
+}
+menuBtn2.addEventListener('click',closeNav)
+
+
+// nav color change scroll
+
+window.addEventListener('scroll',()=>{
+    document.querySelector('nav').classList.toggle('window-scroll',window.scrollY>100)
+})
